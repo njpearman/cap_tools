848128e9367dde422667a7c97f2e3ac4570357de
njp <n.pearman@gmail.com>
2019-03-22 15:00:55 +0100

    Pulls out a separate file for foreman

--- lib/cap_tools.rb
+++ lib/cap_tools.rb
@@ -1,2 +1,3 @@
 puts "My Capistrano tools!"
 load File.join(File.dirname(File.expand_path(__FILE__)), "./cap_tools/tasks/tasks.rake")
+load File.join(File.dirname(File.expand_path(__FILE__)), "./cap_tools/tasks/foreman.rake")
